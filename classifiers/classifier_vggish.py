e52a562e4b62c4600af26ca0081cc40d21de31b4
Boidin Benoît <benoit.bboidin@gmail.com>
2023-12-31 08:38:11 +0100

    Loading vggish representation. Seems to be only one track...

--- classifiers/classifier_vggish.py
+++ classifiers/classifier_vggish.py
@@ -1,44 +1,60 @@
 import pickle
 
-# Train dataset : list of (id, genre, embed)
-data = pickle.load(open('train_id_genres_vgg.pickle','rb'))
 
-ids = [x[0] for x in data]
-genres = [x[1] for x in data]
-embed = [x[2] for x in data]
+def load_data(train_filename='train_id_genres_vgg.pickle', test_filename='test_id_vgg.pickle'):
+    # Train dataset : list of (id, genre, embed)
+    data = pickle.load(open(train_filename,'rb'))
 
-#one hot encoding
-import tensorflow as tf
-genres = [int(x)-1 for x in genres]
-genres = tf.keras.utils.to_categorical(genres)
+    ids = [x[0] for x in data]
+    genres = [x[1] for x in data]
+    embed = [x[2] for x in data]
 
-# Example
-TRACK_NUMBER = 10
-print("track n°", TRACK_NUMBER)
-print("Song genres ", genres[TRACK_NUMBER])
-print("Embed shape ", embed[TRACK_NUMBER].shape)
-print("Embed ", embed[TRACK_NUMBER])
+    #one hot encoding
+    import tensorflow as tf
+    genres = [int(x)-1 for x in genres]
+    genres = tf.keras.utils.to_categorical(genres)
 
-import numpy as np
-x_train = np.array(embed)
-y_train = np.array(genres)
-print(x_train.shape, y_train.shape)
+    # Example
+    TRACK_NUMBER = 10
+    print("track n°", TRACK_NUMBER)
+    print("Song genres ", genres[TRACK_NUMBER])
+    print("Embed shape ", embed[TRACK_NUMBER].shape)
+    print("Embed ", embed[TRACK_NUMBER])
 
-# Test dataset : list of (id, embed)
-data = pickle.load(open('test_id_vgg.pickle','rb'))
+    import numpy as np
+    x_train = np.array(embed)
+    y_train = np.array(genres)
+    print(x_train.shape, y_train.shape)
 
-tmp = [(x[0],x[1]) for x in data if len(x[1]) == 31]
-ids = [x[0] for x in tmp]
-embed = [x[1] for x in tmp]
+    # Test dataset : list of (id, embed)
+    data = pickle.load(open(test_filename,'rb'))
 
-# Example
-TRACK_NUMBER = 10
-print("track n°", TRACK_NUMBER)
-print("Song id ", ids[TRACK_NUMBER])
-print("Embed shape ", embed[TRACK_NUMBER].shape)
-print("Embed ", embed[TRACK_NUMBER])
+    tmp = [(x[0],x[1]) for x in data if len(x[1]) == 31]
+    ids = [x[0] for x in tmp]
+    embed = [x[1] for x in tmp]
 
-import numpy as np
-embed = np.array(embed)
-ids = np.array(ids)
-print(embed.shape, ids.shape)
+    # Example
+    TRACK_NUMBER = 10
+    print("track n°", TRACK_NUMBER)
+    print("Song id ", ids[TRACK_NUMBER])
+    print("Embed shape ", embed[TRACK_NUMBER].shape)
+    print("Embed ", embed[TRACK_NUMBER])
+
+    import numpy as np
+    embed = np.array(embed)
+    ids = np.array(ids)
+    print(embed.shape, ids.shape)
+
+    return x_train, y_train, embed, ids
+
+
+if __name__ == '__main__': 
+    print('Start...')
+
+    x_train, y_train, x_test, ids = load_data('data/train_id_genres_vgg.pickle', 
+                                              'data/test_id_vgg.pickle')
+
+    print(x_train)
+    print(y_train)
+    print(x_test)
+    print(ids)
